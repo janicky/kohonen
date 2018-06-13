98bdff81ab1ca61613f4c8ed5361fb3fa8754ee5
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-13 23:14:16 +0200

    Set weight range

--- src/main/java/Neurone.java
+++ src/main/java/Neurone.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Neurone {
 
-    private List<Double> weights = new ArrayList<>();
+    private List<Weight> weights = new ArrayList<>();
     private List<Point> points;
     private Random random = new Random();
 
@@ -15,33 +15,28 @@ public class Neurone {
     }
 
     private void initWeights() {
-        double range_min = Double.MAX_VALUE;
-        double range_max = 0;
-        for (Point p : points) {
-            if (p.getX() < range_min) {
-                range_min = p.getX();
-            }
-            if (p.getX() > range_max) {
-                range_max = p.getX();
-            }
-            if (p.getY() < range_min) {
-                range_min = p.getY();
-            }
-            if (p.getY() > range_max) {
-                range_max = p.getY();
-            }
-        }
         for (int i = 0; i < points.size(); i++) {
-            double weight = range_min + (range_max - range_min) * random.nextDouble();
-            weights.add(i, weight);
+            weights.add(i, new Weight(0, 1));
         }
     }
 
     private double function(int n) {
-        double weight = weights.get(n);
+        Weight weight = weights.get(n);
         Point point = points.get(n);
 
-        return Math.sqrt(Math.pow(weight - point.getX(), 2) + Math.pow(weight - point.getY(), 2));
+        return 1 / Math.sqrt(Math.pow(weight.getX() - point.getX(), 2) + Math.pow(weight.getY() - point.getY(), 2));
+    }
+
+    public Point getOutput() {
+        Point p_max = null;
+        double max_response = 0;
+        for (Point p : points) {
+            if (max_response < function(points.indexOf(p))) {
+                max_response = function(points.indexOf(p));
+                p_max = p;
+            }
+        }
+        return p_max;
     }
 
 }
