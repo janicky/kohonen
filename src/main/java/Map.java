ab22549c93a1927e8b129b473337a7f86a25fa08
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-13 21:50:22 +0200

    Init neurons in map class

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Map {
 
     private List<Point> points;
+    private List<Neurone> neurons = new ArrayList<>();
     private int size;
 
     public Map(int size) {
@@ -11,5 +13,15 @@ public class Map {
 //        load data from file
         DataLoader dl = new DataLoader("attract_small.txt");
         points = dl.getPoints();
+//        create neurons
+        createNeurons();
+
+        System.out.println(neurons.toString());
+    }
+
+    private void createNeurons() {
+        for (int i = 0 ; i < size; i++) {
+            neurons.add(new Neurone(points));
+        }
     }
 }
