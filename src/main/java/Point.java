b7f037f686e205ca7d6238eddddf3a17f91d4abf
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-14 12:07:28 +0200

    Add neurone to point

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -2,10 +2,15 @@ public class Point {
 
     private double x;
     private double y;
+    private Neurone neurone = null;
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public void setNeurone(Neurone neurone) {
+        this.neurone = neurone;
     }
 
     public double getX() {
