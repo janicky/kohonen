9dfea4cf1735d845913086da3333d73b0ef65b1d
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-14 10:48:21 +0200

    Add epoch to map

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -6,6 +6,8 @@ public class Map {
     private List<Point> points;
     private List<Neurone> neurons = new ArrayList<>();
     private int size;
+    private double lambda;
+    private int epoch = 0;
 
     public Map(int size) {
 //        set map size
@@ -15,8 +17,34 @@ public class Map {
         points = dl.getPoints();
 //        create neurons
         createNeurons();
+//        init lambda
+        lambda = max() / neurons.size();
 
-        System.out.println(neurons.toString());
+        System.out.println(lambda);
+
+        epoch();
+    }
+
+    public void epoch() {
+        for (Neurone n : neurons) {
+            System.out.println(n.getOutput());
+
+        }
+        ++epoch;
+    }
+
+
+    private double max() {
+        double range_max = 0;
+        for (Point p : points) {
+            if (p.getX() > range_max) {
+                range_max = p.getX();
+            }
+            if (p.getY() > range_max) {
+                range_max = p.getY();
+            }
+        }
+        return range_max;
     }
 
     private void createNeurons() {
